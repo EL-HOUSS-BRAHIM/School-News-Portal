f2164a0b05b63f65db050a1e6184141871bb4056
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-01-07 00:20:50 +0100

    adding translation Controller

---
+++ back/core/Translate.php
@@ -0,0 +1,32 @@
+<?php
+class Translate {
+    private static $instance = null;
+    private static $lang = 'fr';
+    private static $translations = [];
+    
+    public static function init() {
+        if (isset($_GET['lang']) && in_array($_GET['lang'], ['fr', 'ar'])) {
+            self::$lang = $_GET['lang'];
+            $_SESSION['lang'] = self::$lang;
+        } elseif (isset($_SESSION['lang'])) {
+            self::$lang = $_SESSION['lang'];
+        }
+        
+        self::loadTranslations();
+    }
+    
+    private static function loadTranslations() {
+        $langFile = __DIR__ . "/../lang/" . self::$lang . ".php";
+        if (file_exists($langFile)) {
+            self::$translations = require $langFile;
+        }
+    }
+    
+    public static function get($key) {
+        return self::$translations[$key] ?? $key;
+    }
+    
+    public static function getCurrentLang() {
+        return self::$lang;
+    }
+}
