98213e183482069bf43d625a1d26c2829690b79b
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-01-07 00:21:32 +0100

    adding translation Languages

---
+++ back/lang/ar.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'home' => 'الرئيسية',
+    'categories' => 'الفئات',
+    'popular' => 'شائع',
+    'trending' => 'رائج',
+    'latest' => 'أحدث المقالات',
+    'featured' => 'مميز', 
+    'view_all' => 'عرض الكل',
+    'follow_us' => 'تابعنا',
+    'newsletter' => 'النشرة الإخبارية',
+    'tags' => 'الوسوم',
+    'quick_links' => 'روابط سريعة',
+    'search' => 'بحث'
+];
---
+++ back/lang/fr.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'home' => 'Accueil',
+    'categories' => 'Catégories',
+    'popular' => 'Populaire',
+    'trending' => 'Tendances',
+    'latest' => 'Derniers Articles',
+    'featured' => 'En Vedette',
+    'view_all' => 'Voir Tout',
+    'follow_us' => 'Suivez-nous', 
+    'newsletter' => 'Newsletter',
+    'tags' => 'Tags',
+    'quick_links' => 'Liens Rapides',
+    'search' => 'Rechercher'
+];
